1acb3b96ba4e0311cd1fcd82619358efcdd5c53c
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-03-10 22:56:05 +1100

    Fix dark theme for clerk components.

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -8,8 +8,13 @@ const SettingsPage = () => {
             appearance={{
                 elements: {
                     rootBox: 'shadow-none w-full',
-                    card: 'border border-accent shadow-nnone w-full'
-
+                    card: 'border border-accent shadow-nnone w-full',
+                    profileSectionPrimaryButton__organizationProfile: 'text-muted-foreground hover:text-foreground',
+                    navbarButtonText: 'text-muted-foreground',
+                    tabButton: 'text-muted-foreground hover:text-foreground aria-[selected=true]:text-foreground group aria-[selected=true]:border-b border-foreground',
+                    notificationBadge: 'text-muted-foreground group-hover:text-foreground group-aria-[selected=true]:text-foreground',
+                    menuButton__member: 'text-muted-foreground hover:text-foreground',
+                    menuItem: 'data-[color=danger]:text-red-500'
                 }
             }}
         />
