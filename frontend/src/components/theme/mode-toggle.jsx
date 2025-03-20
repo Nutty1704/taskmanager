757f37138346653ea393bd8130d6dfcbc6099ee1
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-03-20 15:49:10 +1100

    Modify theme toggle to switch theme on click instead of showing a drop down menu

--- frontend/src/components/theme/mode-toggle.jsx
+++ frontend/src/components/theme/mode-toggle.jsx
@@ -1,34 +1,17 @@
 import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "@/components/ui/dropdown-menu"
 import { useTheme } from "./theme-provider"
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
+  const isDark = theme === 'dark';
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 dark:hidden" />
-          <Moon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:block hidden" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem className='cursor-pointer' onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem className='cursor-pointer' onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div onClick={() => setTheme(isDark ? 'light' : 'dark')}
+      className={`flex items-center cursor-pointer text-foreground transition-transform duration-500 ${isDark ? 'rotate-0' : 'rotate-180'}`}>
+      {isDark
+        ? <Moon className='h-6 w-6 rotate-0 transition-all' />
+        : <Sun className='h-6 w-6 rotate-0 transition-all' />
+      }
+    </div>
   )
 }
